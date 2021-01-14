627507ba6150f57664dabf58a195e84505cae262
Trazz0 <nikolajstunsbjerg@gmail.com>
2021-01-14 08:49:25 +0100

    Added python file

---
+++ ML_test.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+from tensorflow.keras import layers
+from keras.utils import to_categorical
+import numpy as np
+import matplotlib as plt
+
