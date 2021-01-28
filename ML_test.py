cd361e6f0076b6169af0f45840aac35818bbff5f
Trazz0 <nikolajstunsbjerg@gmail.com>
2021-01-28 09:05:32 +0100

    almost finished model

--- ML_test.py
+++ ML_test.py
@@ -26,8 +26,8 @@ for category in categories:
 
 img_size = 100
 new_array = cv2.resize(img_array, (img_size, img_size))
-#plt.imshow(new_array, cmap = "gray")
-#plt.show()
+# plt.imshow(new_array, cmap = "gray")
+# plt.show()
 #print(new_array)
 
 training_data = []
@@ -75,21 +75,21 @@ y = pickle.load(open("y.pickle","rb"))
 
 X = X/255.0
 
-model = Sequential()
-model.add(Conv2D(64, (3,3), input_shape = X.shape[1:]))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
+# model = Sequential()
+# model.add(Conv2D(64, (3,3), input_shape = X.shape[1:]))
+# model.add(Activation("relu"))
+# model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Conv2D(64, (3,3), input_shape = X.shape[1:]))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
+# model.add(Conv2D(64, (3,3), input_shape = X.shape[1:]))
+# model.add(Activation("relu"))
+# model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Flatten())
-model.add(Dense(64))
+# model.add(Flatten())
+# model.add(Dense(64))
 
-model.add(Dense(1))
-model.add(Activation("sigmoid"))
+# model.add(Dense(1))
+# model.add(Activation("sigmoid"))
 
-model.compile(loss="binary_crossentropy", optimizer="adam",metrics=["accuracy"])
+# model.compile(loss="binary_crossentropy", optimizer="adam",metrics=["accuracy"])
 
-model.fit(X, y, batch_size=32, epochs=3, validation_split=0.1)
+# model.fit(X, y, batch_size=32, epochs=3, validation_split=0.1)
