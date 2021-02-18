5c7b883c76fb0a222fe73518d6ba58ef5c358108
Trazz0 <nikolajstunsbjerg@gmail.com>
2021-02-18 16:12:53 +0100

    changed image file path

--- image_test.py
+++ image_test.py
@@ -1,28 +1,13 @@
 import tensorflow as tf
-import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import cv2
 import random
-# X = pickle.load(open("X.pickle","rb"))
-# y = pickle.load(open("y.pickle","rb"))
-# a = random.randint(0, 7990)
-# print(X.shape)
-# for i in range(10):
-#     if y[a] == 0:
-#         print("It is a dog")
-#     else:
-#         print("It is a cat")
-#     plt.imshow(X[a], cmap="gray")
-#     plt.show()
-#     a += 1
-#     # print(X[a])
-
 
 # Predictions for test images
 
-test_data = "F:\\Nextcloud\\assignments\\Odense Tekniske Gymnasium assignments\\OTG 3.C\\Programmering\\git\\Machine_Learning_test\\ML-Test-\\images\\dataset\\test_set\\combined"
+test_data = "\\ML-Test-\\images\\dataset\\test_set\\combined"
 categories = ["dog", "cat"]
 data = []
 img_size = 100
